1d47fcfe1da1de5ea132bec74684aa59616aa0f8
DanE <edwardsdan001@gmail.com>
2018-11-03 22:55:17 -0400

    added functions

---
+++ Lab5.py
@@ -0,0 +1,25 @@
+def TestYN(testInput):
+    while(testInput != 'y' or 'n'):
+        if(testInput == 'y'):
+            return 1
+        elif(testInput == 'n'):
+            return 0
+        else:
+            testInput = input('uwotm8? ')
+
+def ValidateContinue():
+    userInput = input('Would you like to continue? (y/n)')
+    if(TestYN(userInput) == 0):
+        print('Goodbye!')
+        exit()
+    else:
+        print('WOOOOOOOOOOOOOOO!!!!!!')
+
+userInput = int(input('Enter an integer: '))
+factorial = 1
+i = 1
+while (userInput != 1):
+    factorial *= userInput
+    userInput -= 1
+
+print(str(factorial))
