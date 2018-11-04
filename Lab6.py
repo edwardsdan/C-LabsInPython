84d83b79c24658b20184615347ad59b7698a09ae
DanE <edwardsdan001@gmail.com>
2018-11-04 14:41:56 -0500

    Added most functionality

---
+++ Lab6.py
@@ -0,0 +1,48 @@
+import random
+import re
+
+def isValidDice():
+    userInput = input('How many sides for this dice? ')
+    sidesRegex = re.compile('^[468]$|^[1][02468]$|^20$')
+    while not (sidesRegex.match(userInput)):
+        userInput = input('uwotm8? Try again! ')
+    return int(userInput)
+
+def isInt():
+    userInput = input('How many dice do you want to roll? (<=20)')
+    numberRegex = re.compile('^[1-9]$|^1[0-9]$|^20$')
+    while not (numberRegex.match(userInput)):
+        userInput = input('uwotm8? Try again! ')
+    return int(userInput)
+
+def RollDice(diceList):
+    print('Your results: ')
+    for die in diceList:
+        print(random.randint(1,die))
+
+def TestYN(testInput):
+    while(testInput != 'y' or 'n'):
+        if(testInput == 'y'):
+            return 1
+        elif(testInput == 'n'):
+            return 0
+        else:
+            testInput = input('uwotm8? ')
+
+def ValidateContinue():
+    userInput = input('Would you like to continue? (y/n)')
+    if(TestYN(userInput) == 0):
+        print('Goodbye!')
+        exit()
+    else:
+        print('WOOOOOOOOOOOOOOO!!!!!!')
+
+diceList = [0 for i in range(isInt())]
+i = 0
+while (i < len(diceList)):
+    diceList[i] = isValidDice()
+    i += 1
+promptRoll = input('Would you like to roll now? (y/n) ')
+if(TestYN(promptRoll) == 1):
+    RollDice(diceList)
+ValidateContinue()
