1e6301746e5f5868c68d5e91764322f2970c508a
DanE <edwardsdan001@gmail.com>
2018-11-04 17:36:14 -0500

    Made methods static

--- Lab6.py
+++ Lab6.py
@@ -2,29 +2,34 @@ import random
 import re
 
 class DiceRoller:
-    def isValidDice(self):
+    
+    @staticmethod
+    def isValidDice():
         userInput = input('How many sides for this dice? (even, <= 20) ')
         sidesRegex = re.compile('^[468]$|^[1][02468]$|^20$')
         while not (sidesRegex.match(userInput)):
             userInput = input('uwotm8? Try again! ')
         return int(userInput)
 
-    def isInt(self):
+    @staticmethod
+    def isInt():
         userInput = input('How many dice do you want to roll? (<=20) ')
         numberRegex = re.compile('^[1-9]$|^1[0-9]$|^20$')
         while not (numberRegex.match(userInput)):
             userInput = input('uwotm8? Try again! ')
         return int(userInput)
 
-    def RollDice(self, diceList, rolls):
+    @staticmethod
+    def RollDice(diceList, rolls):
         print(f'Roll #{rolls}:')
         for die in diceList:
             print(random.randint(1,die))
-            
-    def AskRollDice(self, diceList, rolls):
+
+    @staticmethod       
+    def AskRollDice(diceList, rolls):
         promptRoll = input('Would you like to roll now? (y/n) ')
         if(TestYN(promptRoll) == 1):
-            self.RollDice(diceList, rolls)
+            DiceRoller.RollDice(diceList, rolls)
             return 1
 
 def TestYN(testInput):
@@ -45,15 +50,14 @@ def ValidateContinue():
         print('WOOOOOOOOOOOOOOO!!!!!!')
 
 def Game():
-    GameRunner = DiceRoller()
     rolls = 1
     while(1):
-        diceList = [0 for i in range(GameRunner.isInt())]
+        diceList = [0 for i in range(DiceRoller.isInt())]
         i = 0
         while (i < len(diceList)):
-            diceList[i] = GameRunner.isValidDice()
+            diceList[i] = DiceRoller.isValidDice()
             i += 1
-        if (GameRunner.AskRollDice(diceList, rolls) == 1):
+        if (DiceRoller.AskRollDice(diceList, rolls) == 1):
             rolls += 1
         ValidateContinue()
 
